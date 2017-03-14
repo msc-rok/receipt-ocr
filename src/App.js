bbaffeab98f54bbd74d5b0c2a989900cbe226e9f
Scheid <marcel.scheid@accenture.com>
2017-03-14 17:43:08 +0100

    local proxy

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import { ReceiptItemList, ReceiptItemEdit, ReceiptItemCreate } from './receiptit
 import { ReceiptList,ReceiptShow } from './receipt';
 var API_ROUTE = process.env.API_ROUTE || "";
 
-console.log(API_ROUTE);
+console.log('##############################################  ' + API_ROUTE);
 
 const App = () => (
     <Admin title="Receipt-OCR Admin" dashboard={Dashboard} restClient={postgrestClient(API_ROUTE)}>
